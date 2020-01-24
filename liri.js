923bdd1acfaedf8ca355b35a9ac41d58900b8a32
mhitchi <mayhitchings@gmail.com>
2020-01-23 20:11:06 -0500

    omdb returning stuff, else returning bullshit or nothing

--- liri.js
+++ liri.js
@@ -1,13 +1,18 @@
 require("dotenv").config();
 
 //DEPENDENCIES
-const keys = require("./keys.js");
 const axios = require("axios");
 const dotenv = require("dotenv");
 const fs = require("fs");
 const inquirer = require("inquirer");
 const moment = require("moment");
-const spotify = require("node-spotify-api");
+const keys = require("./keys.js");
+const spotifyAPI = require("node-spotify-api");
+const spotify = new spotifyAPI(keys.spotify);
+
+//global vars
+let searchType;
+let searchInput;
 
 //inquirer
 inquirer.prompt([
@@ -16,7 +21,7 @@ inquirer.prompt([
     type: "list",
     message: "What are you looking for?",
     name: "searchType",
-    choices: ["concert", "song/artist", "movie", "something lse"]
+    choices: ["song/artist", "concert", "movie", "other"]
   },
   {
     //input
@@ -26,34 +31,96 @@ inquirer.prompt([
   }
 ]).then((response) => {
   console.log(response);
-  let searchType = response.searchType;
-  let searchInput = response.searchInput;
+  searchType = response.searchType;
+  searchInput = response.searchInput;
+}).then(() => {
+  //switch
+  switch(searchType) {
+    case "song/artist":
+      getMusic(searchInput);
+      break;
+    case "concert":
+      getConcert(searchInput);
+      break;
+    case "movie":
+      getMovie(searchInput);
+      break;
+    case "other":
+      getRandom(searchInput);
+      break;
+    default:
+      console.log("pick one, dingus");
+  }
 });
 
+//spotify function
+    //render artist, song name, preview link, album
+    //if no song provided, default to "The Sign" by Ace of Base
+    //use node-spotify-api
+const getMusic = (str) => {
+  console.log("getting music");
+  axios
+    .get(`https://api.spotify.com/v1/search$q=${str}`, {
+      headers: {
+        "Authorization": "Basic"
+      }
+    })
+    .then(function(response) {
+      //TODO throwing error
+      console.log(response.data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    }); 
+}
 
-//switch
-// switch(command) {
-//   case "concert-this":
-//     //search Bands in Town Artist Events API for an artist
-//     //"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-//     //use "codingbootcamp" as app_id
-//     //render venue, location, date of event to terminal
-//     break;
-//   case "spotify-this-song":
-//     //render artist, song name, preview link, album
-//     //if no song provided, default to "The Sign" by Ace of Base
-//     //use node-spotify-api
-//     break;
-//   case "movie-this":
-//     //use axios
-//     //use "trilogy" as api key
-//     //render title, year, IMDB rating, Rotten Tomatoes rating, country, language, plot, actors
-//     //if no movie provided, default to "Mr. Nobody"
-//     break;
-//   case "do-what-it-says":
-//     //use fs package
-//     //take text from random.txt
-//     //call spotify-this-song
-// }
+//concert function
+  //search Bands in Town Artist Events API for an artist
+    //"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+    //use "codingbootcamp" as app_id
+    //render venue, location, date of event to terminal
+const getConcert = (str) => {
+  console.log("getting concert");
+  axios
+    .get(`https://rest.bandsintown.com/artists/${str}/events?app_id=codingbootcamp`)
+    .then(function(response) {
+      //TODO returning empty array
+      console.log(response.data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    }); 
+}
+
+//movie function
+  //use axios
+    //use "trilogy" as api key
+    //render title, year, IMDB rating, Rotten Tomatoes rating, country, language, plot, actors
+    //if no movie provided, default to "Mr. Nobody"
+const getMovie = (str) => {
+  console.log("getting movie");
+  let apikey = "cb792c15";
+  axios
+    .get(`http://www.omdbapi.com/?apikey=${apikey}&t=${str}`)
+    .then(function(response) {
+      //returning stuff!!!
+      console.log(response.data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    }); 
+}
+
+//random function
+  //use fs package
+    //take text from random.txt
+    //call spotify-this-song
+const getRandom = (str) => {
+  console.log("getting random");
+  
+}
+
+
+
 
 //output data to log.txt by appending
