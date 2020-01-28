86535f6d5a4990ff60c368c37bea6a7c51f26a2c
mhitchi <mayhitchings@gmail.com>
2020-01-28 12:21:34 -0500

    cleaned up coments

--- liri.js
+++ liri.js
@@ -71,23 +71,6 @@ inquirer.prompt([
 //spotify function
     //render artist, song name, preview link, album
     //use node-spotify-api
-// const getMusic = (str) => {
-//   //console.log("getting music");
-//   spotify
-//     .search({ type: 'track', query: str , limit: 1})
-//     .then( function(response) {
-//       console.log(`Song: ${str}`);
-//       console.log(`Artist: ${response.tracks.items[0].artists[0].name}`);
-//       console.log(`Album: ${response.tracks.items[0].album.name}`);
-//       console.log(`Preview: ${response.tracks.items[0].preview_url}`);
-
-//       logData(`Spotify search for ${str}.`);
-//     })
-//     .catch( function(err) {
-//       console.log(err);
-//     });
-// }
-
 const getMusic = async (str) => {
   //console.log("getting music");
   try {
@@ -106,26 +89,6 @@ const getMusic = async (str) => {
 //concert function
   //search Bands in Town Artist Events API for an artist
   //render venue, location, date of event to terminal
-// const getConcert = (str) => {
-//   console.log("getting concert");
-//   axios
-//     .get(`https://rest.bandsintown.com/artists/${str}/events?app_id=codingbootcamp`)
-//     .then(function(response) {
-//       //console.log(response.data[0]);
-//       console.log(`Artist: ${response.data[0].artist.name}`);
-//       console.log(`Venue: ${response.data[0].venue.name}`);
-//       console.log(`Lat: ${response.data[0].venue.latitude}`);
-//       console.log(`Lon: ${response.data[0].venue.longitude}`);
-//       let date = moment(response.data[0].datetime).format("MM/DD/YYYY");
-//       console.log(`Date: ${date}`);
-
-//       logData(`OMDB search for ${str}.`);
-//     })
-//     .catch(function(error) {
-//       console.log(error);
-//     }); 
-// }
-
 const getConcert = async (str) => {
   console.log("getting concert");
   try {
@@ -146,30 +109,6 @@ const getConcert = async (str) => {
 
 //movie function
   //render title, year, IMDB rating, Rotten Tomatoes rating, country, language, plot, actors
-// const getMovie = (str) => {
-//   console.log("getting movie");
-//   let apikey = "cb792c15";
-//   axios
-//     .get(`http://www.omdbapi.com/?apikey=${apikey}&t=${str}`)
-//     .then(function(response) {
-//       //returning stuff!!!
-//       //console.log(response.data);
-//       console.log(`Title: ${response.data.Title}`);
-//       console.log(`Year: ${response.data.Year}`);
-//       console.log(`IMDB Rating: ${response.data.Ratings[0].Value}`);
-//       console.log(`Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}`);
-//       console.log(`Country: ${response.data.Country}`);
-//       console.log(`Languages: ${response.data.Language}`);
-//       console.log(`Plot Synopsis: ${response.data.Plot}`);
-//       console.log(`Cast: ${response.data.Actors}`);
-
-//       logData(`OMDB search for ${str}.`);
-//     })
-//     .catch(function(error) {
-//       console.log(error);
-//     }); 
-// }
-
 const getMovie = async (str) => {
   console.log("getting movie");
   let apikey = "cb792c15";
